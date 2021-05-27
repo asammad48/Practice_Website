f78065f13e279c44c326a9230ecf9cdb65aec502
Abdul Sammad <Abdul.1130563@talmeez.pk>
2021-05-27 14:58:39 +0500

    Add project files.

---
+++ Practice_Website/Controllers/blogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practice_Website.Controllers
+{
+    public class blogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Practice_Website/Controllers/faqController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practice_Website.Controllers
+{
+    public class faqController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
